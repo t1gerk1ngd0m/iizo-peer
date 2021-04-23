0302a2c839e4bb0f7da202e99a03d2ef2e17e51f
Taiga Miwa <taiga.miwa888@gmail.com>
2021-04-23 19:42:26 +0900

    rspec設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,19 @@ module IizoPeer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true,
+                       system_specs: false
+    end
   end
 end
