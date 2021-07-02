9fc83f22f7275a1f788e7e580087fa8b6679b783
Taiga Miwa <taiga.miwa888@gmail.com>
2021-07-02 17:51:17 +0900

    古いコードを消す

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,6 @@ Rails.application.routes.draw do
   get 'auth/failure', to: redirect('/')
   get 'signin', to: 'sessions#new'
 
-  namespace :slack_commands do
-    namespace :v1 do
-      resources :iizo, only: [:create] do
-        collection do
-          post :list
-        end
-      end
-    end
-  end
   namespace :slack do
     namespace :commands do
       resources :iizo, only: [:create] do
